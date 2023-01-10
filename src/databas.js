f578f9f89ee94e328bc8db1b60fb43666cc6cb6a
troyfridholt <troyfridholt@hotmail.com>
2023-01-10 15:38:49 +0100

    Update databas.js

--- src/databas.js
+++ src/databas.js
@@ -1,10 +1,59 @@
-const textArray = [
+//const textArray = [
+    
     "In vitae bibendum nibh, non porta sapien. Maecenas nulla eros, dictum eget ante interdum, interdum porttitor arcu. Fusce elementum luctus facilisis. Integer pulvinar facilisis nibh, et tincidunt elit congue id. Ut blandit purus at sapien bibendum, id suscipit magna pulvinar. Vestibulum pulvinar venenatis consequat. Aenean condimentum lorem vitae purus gravida luctus. Quisque hendrerit cursus massa sed convallis. Sed feugiat, odio ut egestas maximus, felis quam pretium nunc, et rhoncus leo lacus eu lectus. Integer at mi a diam lacinia pulvinar vel sit amet nisl. Duis tempor egestas blandit. Nunc feugiat accumsan tortor, a ultrices orci finibus a. Pellentesque aliquam velit aliquet orci suscipit mollis.",
     "text 2",
     "text 3",
     "text 4",
     "text 5"
-];
+//];
 
-
-
+const textArray= [
+    {
+      level: 1,
+      texts: [
+        {
+          text: "What is the capital of Sweden?",
+          answers: {
+            A: "Stockholm",
+            B: "Oslo",
+            C: "Copenhagen"
+          },
+          correctAnswer: "A"
+        },
+        {
+          text: "What is the largest planet in our solar system?",
+          answers: {
+            A: "Earth",
+            B: "Jupiter",
+            C: "Saturn"
+          },
+          correctAnswer: "B"
+        }
+      ]
+    },
+    {
+      level: 2,
+      texts: [
+        {
+          text: "Who painted the Mona Lisa?",
+          answers: {
+            A: "Leonardo da Vinci",
+            B: "Michelangelo",
+            C: "Raphael"
+          },
+          correctAnswer: "A"
+        },
+        {
+          text: "What is the national bird of the USA?",
+          answers: {
+            A: "Eagle",
+            B: "Pelican",
+            C: "Rooster"
+          },
+          correctAnswer: "A"
+        }
+      ]
+    },
+    // fler nivåer och texter
+  ];
+  
